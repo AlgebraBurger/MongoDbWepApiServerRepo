949ad28ab8d28153afcc925d3613a1c0842e9d81
juliusbacosa <juliusbacosa@gmail.com>
2015-11-07 23:16:00 +0800

    added api/customer/xxx-xxxx-xxx

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -20,11 +20,23 @@ namespace MongoDbWepApiServer.Controllers
             var MongoDb = new MongoConnect();            
             collection = MongoDb.db.GetCollection<Customer>("Customers");
         }
-
+        // GET api/customers
         public async Task<List<Customer>> Get()
         {
             var result = await collection.Find(x => x._id != null).ToListAsync();
             return result;
+        }
+
+        // GET api/customers/5
+        public async Task<List<Customer>> Get(string id)
+        {
+            var result = await collection.Find(Builders<Customer>.Filter.Eq("_id", id)).ToListAsync();
+            return result;
+        }
+
+        public void Delegate()
+        {
+            //collection.DeleteManyAsync(x => x.FullName == "Anakinxxx");
         }
     }
 }
