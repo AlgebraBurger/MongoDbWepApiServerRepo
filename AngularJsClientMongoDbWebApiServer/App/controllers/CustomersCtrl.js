6f0b5cbc35397578f148bef75e061e42ee2a4086
juliusbacosa <juliusbacosa@gmail.com>
2015-11-08 02:10:31 +0800

    - angularjs + bootstrap + jquery + aspnetwebapi / C# + mongodb / crud

---
+++ AngularJsClientMongoDbWebApiServer/App/controllers/CustomersCtrl.js
@@ -0,0 +1,65 @@
+﻿angular.module("app")    
+    .constant("baseurl", "http://localhost:54445/api/customers/")
+    .controller("CustomersCtrl", function ($scope, $http, baseurl) {
+    $scope.customers = [];
+    
+
+    var loadCustomers = function () {
+        $http.get(baseurl).success(function (data, status, headers, config) {
+            $scope.customers = data;            
+        }).error(function (data, status, headers, config) { });
+    }
+    loadCustomers();
+
+    $scope.deleteCustomer = function (customer) {
+
+        var id = customer._id;
+        $http.delete(baseurl + id).success(function (data) {
+
+            $.each($scope.customers, function (i) {
+                if ($scope.customers[i]._id === customer._id) {
+                    $scope.customers.splice(i, 1);
+                    return false;
+                }
+            });
+
+        }).error(function (data) {
+
+        });
+    }
+
+    $scope.CreateCustomer = function (customer) {
+        $http.post(baseurl, customer).success(function (data) {
+            loadCustomers();
+            $('#CustomerModal').modal('toggle');
+            $scope.customer = [];
+        }).error(function (error) {
+         
+        }).finally(function () {
+
+        });
+    }
+
+    $scope.editCustomer = function (customer) {
+        $scope.data = [];
+        $('#EditCustomerModal').modal('toggle');
+        $scope.data.customer = customer;
+        
+        
+    }
+
+    $scope.UpdateCustomer = function (customer) {
+        
+        var id = customer._id;
+        $http.put(baseurl + id, customer).success(function (data) {
+            loadCustomers();
+            $('#EditCustomerModal').modal('toggle');
+            $scope.customer = [];
+
+        }).error(function (data) {
+
+        });
+    }
+
+    
+});
---
+++ MongoDbWepApiServer/Controllers/CustomersController.cs
@@ -0,0 +1,77 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDbWepApiServer.DAL;
+using MongoDbWepApiServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+
+namespace MongoDbWepApiServer.Controllers
+{
+    [EnableCors(origins: "http://localhost:57184", headers: "*", methods: "*")]
+    public class CustomersController : ApiController
+    {
+
+        public IMongoCollection<Customer> collection;
+        public CustomersController()
+        {
+            var MongoDb = new MongoConnect();            
+            collection = MongoDb.db.GetCollection<Customer>("Customers");
+        }
+        // GET api/customers
+        public async Task<List<Customer>> GetCustomers()
+        {
+            var result = await collection.Find(x => x._id != null).ToListAsync();
+            return result;
+        }
+
+        // GET api/customers/5
+        public async Task<List<Customer>> GetCustomer(string id)
+        {
+            var result = await collection.Find(Builders<Customer>.Filter.Eq("_id", id)).ToListAsync();
+            return result;
+        }
+
+        // POST: api/Customers        
+        public async Task<IHttpActionResult> PostCustomer(Customer customer)
+        {
+            await collection.InsertOneAsync(new Customer
+            {
+                _id = ObjectId.GenerateNewId().ToString(),
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                YearlyIncome = customer.YearlyIncome,
+                DateJoined = customer.DateJoined
+            });
+
+            return CreatedAtRoute("DefaultApi", new { id = customer._id }, customer);
+        }
+
+        // PUT: api/Customers/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCustomer(string id, Customer customer)
+        {
+            await collection.UpdateManyAsync(Builders<Customer>.Filter.Eq("_id", id), Builders<Customer>.Update.Set("FirstName", customer.FirstName));
+            await collection.UpdateManyAsync(Builders<Customer>.Filter.Eq("_id", id), Builders<Customer>.Update.Set("LastName", customer.LastName));
+            await collection.UpdateManyAsync(Builders<Customer>.Filter.Eq("_id", id), Builders<Customer>.Update.Set("YearlyIncome", customer.YearlyIncome));
+            
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Customers/5
+        public async Task<DeleteResult> DeleteCustomer(string id)
+        {   
+            var result = await collection.DeleteOneAsync(Builders<Customer>.Filter.Eq("_id", id));
+            return result;
+        }
+
+
+    
+    }
+}
